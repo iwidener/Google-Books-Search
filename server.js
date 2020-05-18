336b00012467e1012567f96e091b0ec5c347b98f
Inna Widener <iwide2019@gmail.com>
2020-05-17 22:33:27 -0500

    Mongoose installed and code added to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -9,6 +10,15 @@ dotenv.config();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI || "mongodb://localhost/googlebooks",
+ {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
+.then(() => console.log("DB Connected!"));
 
 // Send every request to the React app
 // Define any API routes before this runs
