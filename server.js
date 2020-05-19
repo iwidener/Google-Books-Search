70f7e3976abb24f2ff82c4bc9a22822037acba96
Inna Widener <iwide2019@gmail.com>
2020-05-19 14:56:47 -0500

    DB is set up and running.

--- server.js
+++ server.js
@@ -11,14 +11,18 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Add routes
+//app.use(routes);
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI || "mongodb://localhost/googlebooks",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
  {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
 })
 .then(() => console.log("DB Connected!"));
+
 
 // Send every request to the React app
 // Define any API routes before this runs
