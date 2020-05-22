fd37ed4b864eedef3d8a6cb802d985abd12ff7b1
Inna Widener <iwide2019@gmail.com>
2020-05-22 17:35:11 -0500

    NavbarSearch is added to the search page.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,7 +1,9 @@
 // import React, { useState,useEffect } from "react";
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
+import NavbarSearch from "../components/NavbarSearch";
 import { Col, Row, Container } from "../components/Grid";
+
 // import { Link } from "react-router-dom";
 
 function Search() {
@@ -11,8 +13,11 @@ function Search() {
                 <Col size="md-12">
                     <Jumbotron>
                         <h2>(React) Google Books Search</h2>
-                        <h5>Search for and Save Books of Interest</h5>
+                        <h4>Search for and Save Books of Interest</h4>
                     </Jumbotron>
+
+                    <NavbarSearch/>
+                    
                 </Col>
             </Row>
         </Container>
